b56d6520c6715ac15eaaf84ef73e60862d528981
Donald Dominic <gotodonald@gmail.com>
2022-06-29 14:12:05 +0100

    added port env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require('next');
 
 
 const dev = process.env.NODE_ENV !== 'production';
-const port = 3000;
+const port = process.env.PORT;
 const app = next({ dev, port });
 const handle = app.getRequestHandler();
 

7ab35369adfa7fd9bc7efc717802e9845bf12cbc
Donald Dominic <gotodonald@gmail.com>
2022-07-22 09:37:31 +0100

    added node version

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,15 +1,3 @@
-import { HeyMessenger } from '../../codes/hey-messenger';
 
-const hey_users = new HeyMessenger();
-const usersList = ['John Doe', 'Jessie Match', 'Jane Doe', 'Nicolas Plum',
-  'Jessie Match', 'Chies Lie', 'Bill Peters'
-]
 
-for(let user of usersList) {
-  hey_users.addNewUser(user);
-}
 
-export default function handler(req, res) {
-  res.json(users);
-  res.end();
-}
